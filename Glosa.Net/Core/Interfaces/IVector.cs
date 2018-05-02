c5611121be4fd50d487775533e908589d67932af
Luis Quinones <luis@complicitMatter.com>
2018-05-02 15:44:18 -0700

    feat(main): add initial method signatures for IVectors

--- Glosa.Net/Core/Interfaces/IVector.cs
+++ Glosa.Net/Core/Interfaces/IVector.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Glosa.Net.Core.Interfaces
 {
-    public interface IVector
+    public interface IVector<T>
     {
+        T AddNew(T vector);
+        void AddSelf(T vector);
+        T SubtractNew(T vector);
+        void SubtractSelf(T vector);
+        T DivideNew(T vector, float f);
+        void DivideSelf(T vector, float f);
+        T MultiplyNew(T vector);
+        void MultiplySelf(T vector, float f);
+        float Dot(T vector, T vector2);
+        T Inverse(T vector);
+        float Heading(T vector);
+        T Reflect(T vector, T vector2);
+        T Refract(T vector, T vector2, float f);
+        T Normalize(T vector);
+        float Magnitude(T vector);
+        string Stringify(T vector);
+        float AngleBetween(T vector, T vector2);
     }
 }
