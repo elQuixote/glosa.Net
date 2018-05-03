7e0d0dd96be8b9dcc2852c391c291e17cb23984f
Luis Quinones <luis@complicitMatter.com>
2018-05-03 10:54:26 -0700

    feat(main): implement Length methods in glosaVector2

--- Glosa.Net/Core/Geometry/GlosaVector2.cs
+++ Glosa.Net/Core/Geometry/GlosaVector2.cs
@@ -9,7 +9,7 @@ using Glosa.Net.Structs;
 
 namespace Glosa.Net.Core.Geometry
 {
-    public struct GlosaVector2 : IVector<GlosaVector2>
+    public struct GlosaVector2 : IVector<GlosaVector2>, ILength<GlosaVector2>, IEquals<GlosaVector2>
     {
         [DllImport("vector.dll")]
         private static extern Vector2 vector2(double x, double y);
@@ -187,6 +187,10 @@ namespace Glosa.Net.Core.Geometry
         public float AngleBetween(GlosaVector2 vector)
         {
             return angleBetween(this.vector, vector.vector);
+        }
+        public float Length()
+        {
+            return this.Magnitude();
         }
     }
 }
