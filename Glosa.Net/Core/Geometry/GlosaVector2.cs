deba1b826c032e0eaae3153e953a1307a624e82c
Luis Quinones <luis@complicitMatter.com>
2018-05-02 17:23:15 -0700

    add(main): add IVector to GlosaVector2 class

--- Glosa.Net/Core/Geometry/GlosaVector2.cs
+++ Glosa.Net/Core/Geometry/GlosaVector2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Glosa.Net.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Glosa.Net.Core.Geometry
 {
-    public class GlosaVector2
+    public class GlosaVector2 : IVector<GlosaVector2>
     {
     }
 }
