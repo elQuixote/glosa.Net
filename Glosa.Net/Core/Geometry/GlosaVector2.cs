ba6b1780600c125710d74e24d464c1c4e57daedc
Luis Quinones <luis@complicitMatter.com>
2018-05-02 17:30:11 -0700

    feat(main): using interop service add reference nim constructor for vector2

--- Glosa.Net/Core/Geometry/GlosaVector2.cs
+++ Glosa.Net/Core/Geometry/GlosaVector2.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Glosa.Net.Structs;
 
 namespace Glosa.Net.Core.Geometry
 {
     public class GlosaVector2 : IVector<GlosaVector2>
     {
+        [DllImport("vector.dll")]
+        private static extern Vector2 vector2(double x, double y);
     }
 }
