e12daff7ec7d0149bac95891bc4db6c4a3f12d52
Luis Quinones <luis@complicitMatter.com>
2018-05-02 17:41:28 -0700

    update(main): refactor reference procs to camelCase from PascalCase

--- Glosa.Net/Core/Geometry/GlosaVector2.cs
+++ Glosa.Net/Core/Geometry/GlosaVector2.cs
@@ -14,21 +14,21 @@ namespace Glosa.Net.Core.Geometry
         [DllImport("vector.dll")]
         private static extern Vector2 vector2(double x, double y);
         [DllImport("vector.dll")]
-        private static extern Vector2 AddNew(Vector2 vector);
+        private static extern Vector2 addNew(Vector2 vector);
         [DllImport("vector.dll")]
-        private static extern Vector2 AddSelf(Vector2 vector);
+        private static extern Vector2 addSelf(Vector2 vector);
         [DllImport("vector.dll")]
-        private static extern Vector2 SubtractNew(Vector2 vector);
+        private static extern Vector2 subtractNew(Vector2 vector);
         [DllImport("vector.dll")]
-        private static extern Vector2 SubtractSelf(Vector2 vector);
+        private static extern Vector2 subtractSelf(Vector2 vector);
         [DllImport("vector.dll")]
-        private static extern Vector2 DivideNew(Vector2 vector, float f);
+        private static extern Vector2 divideNew(Vector2 vector, float f);
         [DllImport("vector.dll")]
-        private static extern Vector2 DivideSelf(Vector2 vector, float f);
+        private static extern Vector2 divideSelf(Vector2 vector, float f);
         [DllImport("vector.dll")]
-        private static extern Vector2 MultiplyNew(Vector2 vector, float f);
+        private static extern Vector2 multiplyNew(Vector2 vector, float f);
         [DllImport("vector.dll")]
-        private static extern Vector2 MultiplySelf(Vector2 vector, float f);
+        private static extern Vector2 multiplySelf(Vector2 vector, float f);
         [DllImport("vector.dll")]
         private static extern Vector2 cross(Vector2 vector, Vector2 vector2);
     }
