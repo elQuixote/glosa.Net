e2a56e5284f325f7f29d6b3f6371cfb6e6d8ab1a
Luis Quinones <luis@complicitMatter.com>
2018-05-02 17:25:26 -0700

    feat(main): add Vector2 struct file and constructor

---
+++ Glosa.Net/Structs/Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glosa.Net.Structs
+{
+    public struct Vector2
+    {
+        public double x;
+        public double y;
+
+        public Vector2(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
