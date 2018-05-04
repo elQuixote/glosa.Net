549723f8551bbc57ef84f251a34c8acf08ffd25a
Luis Quinones <luis@complicitMatter.com>
2018-05-04 16:48:31 -0700

    feat(main): add all interfaces to glosaVector3

--- Glosa.Net/Core/Geometry/GlosaVector3.cs
+++ Glosa.Net/Core/Geometry/GlosaVector3.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Glosa.Net.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Glosa.Net.Core.Geometry
 {
-    public class GlosaVector3
+    public struct GlosaVector3 : IVector<GlosaVector2>, ILength<GlosaVector2>, IEquals<GlosaVector2>, IString<GlosaVector2>, ICompare<GlosaVector2>,
+        IClear<GlosaVector2>, IDimension<GlosaVector2>, IHash<GlosaVector2>, ICopy<GlosaVector2>
     {
     }
 }
