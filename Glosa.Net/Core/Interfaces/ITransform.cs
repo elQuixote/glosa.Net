1deb013969aabc1e01a71e8610de209f8f8d49eb
Luis Quinones <luis@complicitMatter.com>
2018-05-16 11:26:26 -0700

    feat(main) add ITransform interface methods, update gitignore file

---
+++ Glosa.Net/Core/Interfaces/ITransform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glosa.Net.Core.Interfaces
+{
+    public interface ITransform<T>
+    {
+        T ScaleNew(double s);
+        void ScaleSelf(double s);
+        T ScaleNew(double sx, double sy, double sz, double sw);
+        void ScaleSelf(double sx, double sy, double sz, double sw);
+        T RotateNew(T vector, float theta);
+        void RotateSelf(float theta);
+        T Translate(T vector1, T vector2);
+        //Need to add Matrix33 and Matrix44 structs before adding transformNew & Self
+    }
+}
