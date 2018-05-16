a12a608ee3a7971cfa5e29b20116bf8f2b9cd874
Luis Quinones <luis@complicitMatter.com>
2018-05-16 14:22:41 -0700

    feat(main): add file stubs for matrix33 and IMatrix, add method signatures for IMatrix interface

---
+++ Glosa.Net/Core/Geometry/Matrix33.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Glosa.Net.Core.Interfaces;
+
+namespace Glosa.Net.Core.Geometry
+{
+    /// <summary>
+    /// Implements a simple row-major 2d matrix (3x3) matrix struct
+    /// </summary>
+    public struct Matrix33 : IEquals<GlosaVector2>, IString<GlosaVector2>, ICompare<GlosaVector2>,
+        IClear<GlosaVector2>, IDimension<GlosaVector2>, IHash<GlosaVector2>, ICopy<GlosaVector2>
+    {
+
+    }
+}
---
+++ Glosa.Net/Core/Interfaces/IMatrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glosa.Net.Core.Interfaces
+{
+    public interface IMatrix<T>
+    {
+        T TransposeNew(T matrix);
+        void TransposeSelf(T matrix);
+        double Determinant(T matrix);
+        T InvertNew(T matrix);
+        void InvertSelf(T matrix);
+        void Set(double n);
+    }
+}
