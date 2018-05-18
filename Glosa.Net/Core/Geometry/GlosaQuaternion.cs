d0b8e0a9ca98d688bc5a253e5417bd7ab15b832b
Luis Quinones <luis@complicitMatter.com>
2018-05-18 14:19:47 -0700

    feat(main): add file stub for GlosaQuaternion

---
+++ Glosa.Net/Core/Geometry/GlosaQuaternion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Glosa.Net.Core.Interfaces;
+
+namespace Glosa.Net.Core.Geometry
+{
+    public struct GlosaQuaternion
+    {
+
+    }
+}
