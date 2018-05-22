adf343f631c7ea2fe2e454d6d4c762641de244f3
Luis Quinones <luis@complicitMatter.com>
2018-05-22 13:53:47 -0700

    feat(main): add file stub and constructor for GlosaPolyline

---
+++ Glosa.Net/Core/Geometry/GlosaPolyline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glosa.Net.Core.Geometry
+{
+    public struct GlosaPolyline
+    {
+        public List<GlosaVector3> vertices;
+        public List<GlosaLineSegment> segments;
+
+        public GlosaPolyline(List<GlosaVector3> verts, List<GlosaLineSegment> segments)
+        {
+            this.vertices = verts;
+            this.segments = segments;
+        } 
+    }
+}
