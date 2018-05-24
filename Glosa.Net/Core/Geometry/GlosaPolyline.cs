e4894fe78d0532a47ae8fd106a5ca07cda5f2efa
Luis Quinones <luis@complicitMatter.com>
2018-05-24 14:31:46 -0700

    feat(main): add helper abstract GlosaObjectclass, update GLS to take IVector interface parameter, update GPL to reflect changes

--- Glosa.Net/Core/Geometry/GlosaPolyline.cs
+++ Glosa.Net/Core/Geometry/GlosaPolyline.cs
@@ -4,26 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using Glosa.Net.Core.Interfaces;
+using Glosa.Net.Core.Helpers;
 
 namespace Glosa.Net.Core.Geometry
 {
-    public struct GlosaPolyline
+    public class GlosaPolyline : GlosaObject
     {
-        [DllImport("wrapper_path.dll")]
-        private static extern GlosaPolyline createPolyline(List<GlosaLineSegment> segments);
+        public IVector[] vertices;
+        public GlosaLineSegment[] segments;
+        public bool closed;
 
-        public List<GlosaVector3> vertices;
-        public List<GlosaLineSegment> segments;
+        public GlosaPolyline()
+        {
 
-        public GlosaPolyline(List<GlosaVector3> verts, List<GlosaLineSegment> segments)
+        }
+        public GlosaPolyline(IVector[] verts, bool closed)
         {
             this.vertices = verts;
-            this.segments = segments;
-        } 
+            this.closed = closed;
+            GenerateSegmentsFromVertices(verts);
+        }
 
-        public static GlosaPolyline CreatePolyline(List<GlosaLineSegment> segs)
+        public GlosaPolyline(GlosaLineSegment[] segments)
         {
-            return createPolyline(segs);
+            this.segments = segments;
+            GenerateVerticesFromSegments(segments);
+        }
+
+        private void GenerateVerticesFromSegments(GlosaLineSegment[] segments)
+        {
+            this.vertices = new IVector[this.segments.Length];
+            int count = 0;
+            foreach (GlosaLineSegment l in segments)
+            {
+                this.vertices[count] = l.startVertex;
+                this.vertices[count] = l.endVertex;
+                count++;
+            }
+        }
+
+        private void GenerateSegmentsFromVertices(IVector[] vertices)
+        {
+            this.segments = new GlosaLineSegment[this.vertices.Length];
+            int count = 0;
+            for(int i = 0; i < vertices.Length -1; i++)
+            {
+                this.segments[count] = (new GlosaLineSegment(vertices[count], vertices[count + 1]));
+                if (this.closed)
+                {
+                    this.segments[count+1] = (new GlosaLineSegment(vertices[vertices.Length-1], vertices[0]));
+                }
+                count++;
+            }
         }
     }
 }
---
+++ Glosa.Net/Core/Helpers/GlosaObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Glosa.Net.Core.Helpers
+{
+    /// <summary>
+    /// The abstract Object Data Class
+    /// </summary>
+    public abstract class GlosaObject
+    {
+        /// <summary>
+        /// Populates the object from Json formatted string
+        /// </summary>
+        /// <param name="data"></param>
+        public void PopulateData(string data)
+        {
+            JsonConvert.PopulateObject(data, this);
+        }
+        /// <summary>
+        /// Serializes the object into a Json formatted string
+        /// </summary>
+        /// <returns>The serialized string</returns>
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+        }
+        private string AddBody(string current_string)
+        {
+            return "{ \"body\": " + current_string + " }";
+        }
+    }
+}
