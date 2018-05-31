a624385e08d9efd73c96fd0607f643532e7ec005
Luis Quinones <luis@complicitMatter.com>
2018-05-31 15:45:20 -0700

    feat(main): add file stub and constructors for GlosaPolygon

---
+++ Glosa.Net/Core/Geometry/GlosaPolygon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using Glosa.Net.Core.Interfaces;
+using Glosa.Net.Core.Helpers;
+using Glosa.Net.Core.Helpers.Json;
+
+namespace Glosa.Net.Core.Geometry
+{
+    public class GlosaPolygon : GlosaObject
+    {
+
+        public GlosaPolyline polyline;
+
+        public GlosaPolygon(IVector[] verts)
+        {
+
+        }
+
+        public GlosaPolygon(GlosaLineSegment[] segments)
+        {
+
+        }
+
+        public GlosaPolygon(GlosaPolyline polyline)
+        {
+            this.polyline = polyline;
+        }
+    }
+}
